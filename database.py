4dae66b5edf6e9bebf253360698ebeaf0db9edf5
swamy-pega <swamy.p@hotmail.com>
2025-11-16 23:46:35 -0500

    db

--- database.py
+++ database.py
@@ -22,28 +22,30 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 DB_HOST = os.getenv("DB_HOST")
 
+DB_NAME = os.getenv("DB_NAME", "postgres")
+INSTANCE_CONNECTION_NAME = os.getenv("INSTANCE_CONNECTION_NAME", "turing-diode-477800-b0:northamerica-northeast2:postgresreact")
+
+
+#DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?host=/cloudsql/{INSTANCE_CONNECTION_NAME}"
+
+
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-DATABASE_URL1 = (
-    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}"
-    f"?host={DB_HOST}"
-)
-print(" DB URL:1 "+DATABASE_URL1) 
-DATABASE_URL = (
-    f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?host={DB_HOST}"
-)  
 
-DATABASE_URL = (
-    f"postgresql+psycopg2://postgres:{DB_PASSWORD}@/postgres"
-    "?host=/cloudsql/turing-diode-477800-b0:northamerica-northeast2:postgresreact"
-)
-DATABASE_URL=settings.db_url
+#DATABASE_URL = (f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?host={DB_HOST}")  
+
+#DATABASE_URL = (
+    #f"postgresql+psycopg2://postgres:{DB_PASSWORD}@/postgres"
+    #"?host=/cloudsql/turing-diode-477800-b0:northamerica-northeast2:postgresreact")
+#DATABASE_URL=settings.db_url
+#print(" DB URL: "+DATABASE_URL) 
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@/{DB_NAME}?host=/cloudsql/{INSTANCE_CONNECTION_NAME}"
 print(" DB URL: "+DATABASE_URL) 
-
 
 #m/swamy-pega/python_api.git
 engine = create_engine(DATABASE_URL)
+print("Engine created successfully"+str(engine))
 #turing-diode-477800-b0:northamerica-northeast2:postgresreact
 # Session setup
 SessionLocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
